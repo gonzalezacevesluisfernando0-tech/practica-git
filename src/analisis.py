fd24ddebbea3e6ff19c4254656e9d765bc53ecf6
Luis Fernando Aceves <gonzalezacevesluisfernando0@gmail.com>
2026-09-03 20:13:56 -0600

    Modificar archivo analisis.py,Implementar valores minimos y maximos de la lista y Cambiar impresión por pantalla del promedio

--- src/analisis.py
+++ src/analisis.py
@@ -1,5 +1,12 @@
 from statistics import mean
 
+# Calculate the average of a list of numbers
 numbers = [10,20,30,40,50]
 avg = mean(numbers)
-print(avg)
+print(f"Average value: {avg}")
+
+# Calculate the minimum and maximum values of the list
+min_value = min(numbers)
+max_value = max(numbers)
+print(f"Minimum value: {min_value}")
+print(f"Maximum value: {max_value}")
